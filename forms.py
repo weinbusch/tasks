cd273cc1638becca8f327753f49e72a7bce5d0e9
Martin Bierbaum <martin.bierbaum@rub.de>
2020-05-26 21:55:14 +0200

    black

--- forms.py
+++ forms.py
@@ -1,22 +1,21 @@
 from wtforms.fields import SelectField
 
-class ModelSelectField(SelectField):
 
+class ModelSelectField(SelectField):
     def __init__(self, query=None, empty=False, empty_label="----", **kwargs):
-        
+
         choices = [(obj.id, str(obj)) for obj in query]
-        
+
         if empty:
             choices.append((None, empty_label))
-        
+
         kwargs["choices"] = choices
-        
+
         def _coerce(value):
             if value:
                 return int(value)
             return None
-            
+
         kwargs["coerce"] = _coerce
-      
+
         super().__init__(*args, **kwargs)
-        
