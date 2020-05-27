b80af54b26c1a963335afa4eeac274edbce7c368
Martin Bierbaum <weinbusch@yahoo.de>
2020-05-27 10:08:41 +0200

    Create documents.py

---
+++ documents.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import relationship
+
+
+Base = declarative_base()
+
+
+class Document(Base):
+    '''
+    Document wrapper containing metadata and a link to the actual file
+    '''
+    pass
+    
+    
+class File(Base):
+    '''
+    File stored on the file system
+    '''
+    id = Column(Integer, primary_key=True)
+    created = Column(DateTime, default=datetime.utcnow())
+    path = Column(String)
+    
+    def read(self):
+        with open(self.path, 'r') as f:
+            return f.read()
+            
+    def write(self, data):
+        with open(self.path, 'wb') as f:
+            f.write(data)
